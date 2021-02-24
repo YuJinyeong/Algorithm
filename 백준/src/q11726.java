df56d5c93f0f4504a6a79ec9b71d380aafef3435
jinyeong <really_real@naver.com>
2021-02-24 13:18:16 +0900

    2×n 타일링

--- 백준/src/q11726.java
+++ 백준/src/q11726.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 
+//Q.11726 2×n 타일링
 public class q11726 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[1001];
 		
+		arr[1] = 1;
+		arr[2] = 2;
+		for(int i=3; i<=N; i++) {
+			arr[i] = (arr[i-1] + arr[i-2])%10007;
+		}
+		
+		System.out.println(arr[N]);
 	}
 }
